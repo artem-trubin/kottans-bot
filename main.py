f00fde4e9cd73a4fce71fcf08e702753db788cc6
artem-trubin <17trubin@gmail.com>
2022-08-04 22:07:47 +0300

    Added htaq and faq messages

--- main.py
+++ main.py
@@ -17,8 +17,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if message.content.startswith("$htaq"):
+        await message.channel.send(
+            """
+Пропонуємо нашу інструкцію про те, як краще задавати питання!
+https://github.com/kottans/frontend/blob/2022_UA/HTAQ.md
+        """
+        )
+
+    if message.content.startswith("$faq"):
+        await message.channel.send(
+            """
+Щоб продивитись популярні питання перейдіть за цим посиланням:
+https://github.com/kottans/frontend/blob/2022_UA/faq.md
+            """
+        )
 
 
 client.run(os.getenv("TOKEN"))
